176c284f94d91f235ab48355c09b39b31a7d15fe
Balaji Sridharan <ninjabigdata@gmail.com>
2021-08-30 12:54:43 +0530

    Initial data for Beer table

---
+++ mssc-beer-service/src/main/java/balaji/springframweork/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package balaji.springframweork.msscbeerservice.bootstrap;
+
+import balaji.springframweork.msscbeerservice.domain.Beer;
+import balaji.springframweork.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if (beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(584841897L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(584841888L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        System.out.println("Loaded beers: " + beerRepository.count());
+    }
+}
